7b7086f3f0da0aa1c7dca5c76f64ba4e9e97f693
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2026-02-16 11:34:35 -0300

    refactor(auth): add failure import

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:cortex_bank_mobile/core/models/user.dart';
+import 'package:cortex_bank_mobile/core/utils/failure.dart';
 import 'package:cortex_bank_mobile/core/utils/result.dart';
 import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_local_datasource.dart';
 import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_remote_datasource.dart';
