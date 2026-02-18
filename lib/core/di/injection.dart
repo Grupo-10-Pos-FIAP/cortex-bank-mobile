08affdc95eb15df7c7ddd4b801efebcbfe8aa8d6
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2026-02-18 18:45:05 -0300

    refactor(di): use Firebase auth in DI

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -2,22 +2,25 @@ import 'package:get_it/get_it.dart';
 import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_local_datasource.dart';
 import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_local_datasource_in_memory.dart';
 import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_remote_datasource.dart';
-import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_remote_datasource_fake.dart';
+import 'package:cortex_bank_mobile/features/auth/data/datasources/auth_datasource_firebase.dart';
 import 'package:cortex_bank_mobile/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:cortex_bank_mobile/features/auth/data/repositories/i_auth_repository.dart';
 import 'package:cortex_bank_mobile/features/transaction/data/datasources/transactions_datasource.dart';
 import 'package:cortex_bank_mobile/features/transaction/data/datasources/transactions_datasource_in_memory.dart';
 import 'package:cortex_bank_mobile/features/transaction/data/repositories/i_transactions_repository.dart';
 import 'package:cortex_bank_mobile/features/transaction/data/repositories/transactions_repository_impl.dart';
+import 'package:cortex_bank_mobile/services/firebase_service.dart';
 
 final getIt = GetIt.instance;
 
 void configureDependencies() {
+  getIt.registerLazySingleton<DatabaseService>(() => FirestoreService());
+
   getIt.registerLazySingleton<AuthLocalDataSource>(
     () => AuthLocalDataSourceInMemory(),
   );
   getIt.registerLazySingleton<AuthRemoteDataSource>(
-    () => AuthRemoteDataSourceFake(),
+    () => AuthDatasourceFirebase(),
   );
   getIt.registerLazySingleton<IAuthRepository>(
     () => AuthRepositoryImpl(getIt(), getIt()),
