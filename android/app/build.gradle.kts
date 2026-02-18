5128fb60b925033b58f7260756c309ba9f3fe675
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2026-02-18 18:41:09 -0300

    chore(android): tune manifest and build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,10 +36,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.cortex_bank_mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -48,8 +45,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Para publicar na Play Store: crie um keystore e configure signingConfigs.release
+            // Local: usa debug. Para publicar: configure signingConfigs.release (ver android/PRODUCAO.md)
             signingConfig = signingConfigs.getByName("debug")
         }
     }
